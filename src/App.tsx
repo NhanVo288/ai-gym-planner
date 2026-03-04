4dcd524801785ed374d8ac3a9d0d6f342073cc31
vnhan28 <vnhan2808@gmail.com>
2026-03-04 14:57:41 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Suspense, lazy } from "react"
+
+
+const Home = lazy(() => import("./pages/Home"))
+const Onboarding = lazy(() => import("./pages/Onboarding"))
+const Profile = lazy(() => import("./pages/Profile"))
+const Auth = lazy(() => import("./pages/Auth"))
+const Account = lazy(() => import("./pages/Account"))
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/onboarding" element={<Onboarding />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/auth/:pathname" element={<Auth />} />
+          <Route path="/account/:pathname" element={<Account />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   )
 }
 
